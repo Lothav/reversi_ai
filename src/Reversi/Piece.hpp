c0fa395fe38fecb44b01554cda3f7f9f27719060
Luiz0tavio <me@luizotav.io>
2018-11-09 13:47:46 -0200

    table constructor

--- src/Reversi/Piece.hpp
+++ src/Reversi/Piece.hpp
@@ -1,9 +1,21 @@
 #ifndef REVERSEAI_PIECE_HPP
 #define REVERSEAI_PIECE_HPP
 
+enum PieceType{
+    EMPTY,
+    WHITE,
+    BLACK
+};
+
 class Piece
 {
+private:
 
+    PieceType type = PieceType::EMPTY;
+
+public:
+
+    Piece() = default;
 };
 
 #endif //REVERSEAI_PIECE_HPP
--- src/Reversi/Table.hpp
+++ src/Reversi/Table.hpp
@@ -1,11 +1,28 @@
 #ifndef REVERSEAI_TABLE_HPP
 #define REVERSEAI_TABLE_HPP
 
+#include <array>
+#include "Piece.hpp"
 
 class Table
 {
 
-};
+private:
 
+    std::array<std::array<Piece*, 8>, 8> pieces = {};
+
+public:
+
+    Table()
+    {
+        for (auto &lines: pieces){
+            for (auto &cells: lines) {
+                cells = new Piece();
+            }
+        }
+    }
+
+
+};
 
 #endif //REVERSEAI_TABLE_HPP
